df16753828d0737b4836af44e1a1410fad068082
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-10-16 15:44:01 -0500

    java exercise

---
+++ src/helloworld.java
@@ -0,0 +1,32 @@
+public class helloworld {
+    public static void main(String [] args){
+        System.out.println("hello world");
+
+        int myFavoriteNumber= 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Alexandra";
+        System.out.println(myString);
+
+        char charValue= 'A';
+        System.out.println(charValue);
+
+        double decimal= 3.1459;
+        System.out.println(decimal);
+
+        float myNumber=3.14f;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x2 = 5;
+        System.out.println(++x2);
+        System.out.println(x2);
+
+
+
+
+    }
+}
+
